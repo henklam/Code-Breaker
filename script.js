9afe83119fdf9c3e7a520d305589b6d794bd6150
henklam <183881683+henklam@users.noreply.github.com>
2024-11-13 01:28:17 +0000

    added extra menu with local storage stats

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ var random1 = Math.floor(Math.random()*3)+1;
 var random2 = Math.floor(Math.random()*3)+1;
 var random3 = Math.floor(Math.random()*3)+1;
 var compNums = [random1, random2, random3];
+console.log(compNums);
 var nums = [];
+var infoShow = false;
 
 document.getElementById("clock").innerHTML = "Clock: " + clock;
 
+if(localStorage.getItem("scores") == null) {
+    var scores = [0,0];
+    localStorage.setItem("scores", JSON.stringify(scores));
+} else {
+    var scores = JSON.parse(localStorage.getItem("scores"));
+}
+
 function clickOne() {
-    if(digit < 4) {
+    if(digit < 4 && !infoShow) {
         document.getElementsByTagName("p")[digit].innerHTML = 1;
         nums[digit-1] = 1;
         digit++;
@@ -18,7 +27,7 @@ function clickOne() {
 }
 
 function clickTwo() {
-    if(digit < 4) {
+    if(digit < 4 && !infoShow) {
         document.getElementsByTagName("p")[digit].innerHTML = 2;
         nums[digit-1] = 2;
         digit++;
@@ -26,7 +35,7 @@ function clickTwo() {
 }
 
 function clickThree() {
-    if(digit < 4) {
+    if(digit < 4 && !infoShow) {
         document.getElementsByTagName("p")[digit].innerHTML = 3;
         nums[digit-1] = 3;
         digit++;
@@ -34,9 +43,9 @@ function clickThree() {
 }
 
 function clickOkay() {
-    if(digit == 4) {
+    if(digit == 4 && !infoShow) {
         match = true;
-        for(let i = 0; i < nums.length; i++) {
+        for(let i = 1; i < nums.length; i++) {
             if(nums[i] != compNums[i]) {
                 match = false;
             }
@@ -49,15 +58,19 @@ function clickOkay() {
     }
     if(match) {
         document.getElementById("msg").innerHTML = "You guessed correctly!";
+        scores.push(8-clock);
+        scores[1]++;
+        localStorage.setItem("scores", JSON.stringify(scores));
     } else if(clock == 0) {
         document.getElementById("msg").innerHTML = "You ran out of time!";
+        scores[0]++;
     }
 }
 
 function clickClear() {
-    if(!match && clock != 0) {
+    if(!match && clock != 0 && !infoShow) {
         nums = [];
-        for(let i = 1; i < 4; i++) {
+        for(let i = 2; i < 4; i++) {
             document.getElementsByTagName("p")[i].innerHTML = "";
         }
         digit = 1;
@@ -68,10 +81,37 @@ function clickClear() {
 }
 
 function eraseOne() {
-    if(!match && clock != 0 && digit > 1) {
+    if(!match && clock != 0 && digit > 1 && !infoShow) {
         digit--;
         nums.splice(nums.length-1,1);
         document.getElementsByTagName("p")[digit].innerHTML = "?";
+    }
+}
+
+function info() {
+    var div = document.getElementById("infoDiv");
+    var backDiv = document.getElementById("backDiv");
+    if(!infoShow) {
+        // div.setAttribute("style", "visibility : visible")
+        // backDiv.setAttribute("style", "visibility: visible");
+        div.classList.add("showInfo");
+        backDiv.classList.add("showBackDiv");
+        console.log(div);
+        infoShow = true;
+        let sum = 0;
+        for(let i = 0; i < scores.length; i++) {
+            sum+=scores[i];
+        }
+        let avg = Math.round((sum/(scores.length-2))*100)/100;
+        document.getElementById("avg").innerHTML = "Average guesses: " + avg;
+        document.getElementById("wins").innerHTML = "Wins: " + scores[1];
+        document.getElementById("losses").innerHTML = "Losses: " + scores[0];
+    } else {
+        // div.setAttribute("style", "visibility: hidden");
+        // backDiv.setAttribute("style", "visibility: hidden");
+        div.classList.remove("showInfo");
+        backDiv.classList.remove("showBackDiv");
+        infoShow = false;
     }
 }
 
